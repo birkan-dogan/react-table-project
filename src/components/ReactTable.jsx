b92c97acde6e876b12a2d2f4b10c729bee17d5f8
birkan <b.561birkan@gmail.com>
2022-10-16 12:53:28 +0300

    taking data from api,columns comes to create table

--- src/components/ReactTable.jsx
+++ src/components/ReactTable.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import { useTable } from "react-table";
+import { COLUMNS } from "../helpers/columns";
 const ReactTable = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios("https://fakestoreapi.com/products");
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
+
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => products);
+
+  const tableInstance = useTable({ columns, data });
+
   return <div>ReactTable</div>;
 };
 
---
+++ src/helpers/columns.js
@@ -0,0 +1,14 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Price",
+    accessor: "price",
+  },
+  {
+    Header: "Title",
+    accessor: "title",
+  },
+];
